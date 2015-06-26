d5becd1a7d9808588a5f7a50683d49b8c61a7190
jesusrodriguez <jesus@MacBook-Pro-de-Jesus.local>
2015-06-26 18:18:04 +0200

    editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,8 +64,6 @@ exports.new = function(req, res) {
 // POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
-	console.log("QUIZ VALE: " + quiz);
-	console.log("QUIZ VALIDADO ES: " + quiz.validate());
   
   quiz
   .validate()
@@ -78,6 +76,33 @@ exports.create = function(req, res) {
         .save({fields: ["pregunta", "respuesta"]})
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  ).catch(function(error){next(error)});
+};
+
+// GET / quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT/ quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // Redireccion HTTP a la lista de preguntas(URL relativo)
     }
   ).catch(function(error){next(error)});
 };
